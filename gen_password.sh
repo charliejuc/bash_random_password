014a1617175bf07d91eda934ee0294572cccc020
root <root@charliejuc>
2018-06-04 13:21:01 +0200

    Script for random password generation.

---
+++ gen_password.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+LEN=$1
+
+if [ ! $LEN ]; then
+	>&2 echo "Length parameter is required."
+	exit 1
+fi
+
+UR_LEN=$(($LEN/2))
+R_LEN=$UR_LEN
+
+PASSWORD=$(echo "$(head -c $UR_LEN /dev/urandom)$(head -c $R_LEN /dev/random)" | base64 | cut -c -$LEN)
+
+echo $PASSWORD
